b50ed6ef4f2ecf60d3a64d447f897c691a585020
Britaliope <britaliope@sd-101270.dedibox.fr>
2017-05-09 20:24:42 +0200

    completing classes

--- src/personSystem/Manager.java
+++ src/personSystem/Manager.java
@@ -1,5 +1,12 @@
 package personSystem;
 
 public class Manager extends SystemUser{
+	
+        public Manager(String firstName, String lastname, String password, String nickname){
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.password = password;
+                this.nickname = nickname;
+        }
 
 }
--- src/personSystem/Player.java
+++ src/personSystem/Player.java
@@ -1,5 +1,23 @@
 package personSystem;
 
 public class Player extends SystemUser {
+	private int wallet=0;
+
+	private betSystem.Bet[] betList;
+
+	public Player(String firstName, String lastname, String password, String nickname){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.nickname = nickname;
+	}	
+
+	public void setWallet(int w){
+		this.wallet = w;
+	}
+
+	public int getWallet(){
+		return wallet;
+	}
 
 }

fc73690dcf3de34fde9a4b6a575059d1828d4186
Bruno MATEU <bmateu@pc-df-806.priv.enst-bretagne.fr>
2017-05-09 11:50:52 +0200

    class completion started

--- src/personSystem/Person.java
+++ src/personSystem/Person.java
@@ -1,15 +1,25 @@
 package personSystem;
 
-public class Person {
+abstract class Person {
 	
 	protected String firstName;
 	protected String lastName;
 	
-	public Person(String firstName, String lastName){
-		this.firstName = firstName;
-		this.lastName = lastName;
-		
+
+	public void setFirstName(String name) {
+		firstName = name;
 	}
 	
+	public void setLastName(String name) {
+		lastName = name;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
 
 }
--- src/personSystem/SystemUser.java
+++ src/personSystem/SystemUser.java
@@ -1,5 +1,25 @@
 package personSystem;
 
-public class SystemUser extends Person{
+abstract class SystemUser extends Person{
+	
+	protected String nickname;
+	protected String password;
+	
+	public String getNickname(){
+		return nickname;
+	}
+	
+	public String getPassword(){
+		return password;
+	}
+
+	
+	public void authenticate(String str){
+		//TODO
+	}
+	
+	public void updatePassword(String str){
+		//TODO
+	}
 
 }

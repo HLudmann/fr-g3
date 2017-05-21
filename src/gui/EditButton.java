d8fdddaae3513c853a40ff1339438b92ab9d1f3e
JB Valladeau <jbvalladeau@gmail.com>
2017-05-21 15:04:55 +0200

    Missing file

---
+++ src/gui/EditButton.java
@@ -0,0 +1,46 @@
+import javax.swing.JButton;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class EditButton extends JButton implements MouseListener{
+  private String name;
+  private String data; // The data related to the entry, and that will be send if needed to the backend
+  private int type; // The function from the backend that we need to interact with
+  private int id; // The button id, not really needed tbh
+
+
+  public EditButton(String str, int id, int type, String data) {
+    super(str);
+
+    this.name = str;
+    this.id = id;
+    this.data = data;
+    this.type = type;
+
+    this.addMouseListener(this);
+  }
+
+  //Send the correct instruction based on the type provided
+  public void mouseClicked(MouseEvent event) {
+    switch(type){
+      case 1:
+        System.out.println("data: " + this.data + " type: " + this.type);
+        break;
+      case 2:
+        System.out.println("data: " + this.data + " type: " + this.type);
+        break;
+      case 3:
+        System.out.println("data: " + this.data + " type: " + this.type);
+        break;
+    }
+  }
+
+  //Javax needs this soooo ...
+  public void mouseEntered(MouseEvent event) {}
+
+  public void mouseExited(MouseEvent event) {}
+
+  public void mousePressed(MouseEvent event) {}
+
+  public void mouseReleased(MouseEvent event) {}
+}

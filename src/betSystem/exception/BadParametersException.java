f22c3d733f6f10682299381c0bfd1aed232f8b08
jdesmazi <juliendesmaze@gmail.com>
2017-05-22 11:45:04 +0200

    Add files via upload

---
+++ src/betSystem/exception/BadParametersException.java
@@ -0,0 +1,13 @@
+package betSystem.exception;
+
+public class BadParametersException extends Exception{
+	private static final long serialVersionUID = -385186352928310148L;
+
+	public BadParametersException() {
+        super();
+     }
+	
+      public BadParametersException(String reason) {
+        super(reason);
+     }
+}

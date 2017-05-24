b708292e3e5669332937eda4ad57f79510f43399
JB Valladeau <jbvalladeau@gmail.com>
2017-05-24 16:22:14 +0200

    New panel for the creation AND edition of bets

--- src/gui/PanelFormBet.java
+++ src/gui/PanelFormBet.java
@@ -1,41 +1,124 @@
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JFormattedTextField;
+import java.awt.GridLayout;
 import java.awt.Dimension;
+import javax.swing.border.EmptyBorder;
 import java.util.ArrayList;
 import java.text.NumberFormat;
-import javax.swing.BoxLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingUtilities;
 
 public class PanelFormBet extends JPanel {
-  public PanelFormBet(){
+
+  private int type;
+  private ArrayList<String> competitors;
+
+  private GridLayout gl;
+  private JLabel betSelectLabel;
+  private JLabel betValueLabel;
+  private JLabel firstCompLabel;
+  private JLabel secondCompLabel;
+  private JLabel thirdCompLabel;
+  private JComboBox betSelect;
+  private JComboBox firstCompetitorSelector;
+  private JComboBox secondCompetitorSelector;
+  private JComboBox thirdCompetitorSelector;
+  private JFormattedTextField valueInput;
+  private SendDataButton submit;
+
+  private boolean is_done_painting =false;
+
+  //TODO: add competitors when editing so the user doesn't have to search again
+  public PanelFormBet(boolean is_edition, ArrayList<String> competitors, int value, int type){
+
+    this.type = type;
+    this.competitors = competitors;
 
     //Aesthetic borders
     this.setBorder(new EmptyBorder(10, 10, 10, 10));
 
-    JComboBox betSelect = new JComboBox();
-    betSelect.addItem("Pari podium");
-    betSelect.addItem("Pari simple");
+    this.gl = new GridLayout();
+    this.gl.setColumns(2);
+    this.gl.setRows(6);
+    this.gl.setHgap(5);
+    this.gl.setVgap(5);
+    this.setLayout(this.gl);
 
-    JFormattedTextField value = new JFormattedTextField(NumberFormat.getIntegerInstance());
+    this.valueInput = new JFormattedTextField(NumberFormat.getIntegerInstance());
 
-    JLabel betSelectLabel = new JLabel("Type: ");
-    JLabel betValueLabel = new JLabel("Montant: ");
+    this.betSelectLabel = new JLabel("Type: ");
+    this.betValueLabel = new JLabel("Montant: ");
 
-    JPanel betSelectPanel = new JPanel();
-    betSelectPanel.add(betSelectLabel);
-    betSelectPanel.add(betSelect);
+    this.submit = new SendDataButton("Submit", 1, 1);
 
-    JPanel betValuePanel = new JPanel();
-    betValuePanel.add(betValueLabel);
-    betValuePanel.add(value);
+    this.add(betSelectLabel);
+    if(is_edition){
+      this.add(new JLabel("Non modifiable"));
+    }else{
+      this.betSelect = new JComboBox();
 
-    this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+      this.betSelect.addActionListener (new ActionListener () {
+          public void actionPerformed(ActionEvent e) {
+              update_layout(betSelect.getSelectedIndex());
+          }
+      });
 
-    this.add(betSelectPanel);
-    this.add(betValuePanel);
+      this.betSelect.addItem("Pari podium");
+      this.betSelect.addItem("Pari simple");
+      this.add(this.betSelect);
+    }
+
+    this.firstCompetitorSelector = createAndPopulateCompSelectors();
+    this.secondCompetitorSelector = createAndPopulateCompSelectors();
+    this.thirdCompetitorSelector =  createAndPopulateCompSelectors();
+    this.firstCompLabel = new JLabel("Compétiteur: ");
+    this.secondCompLabel = new JLabel("Deuxième compétiteur: ");
+    this.thirdCompLabel = new JLabel("Troisième compétiteur: ");
+
+    addBottomStuff();
+
+    this.is_done_painting = true;
+
+  }
+
+  public JComboBox createAndPopulateCompSelectors(){
+    JComboBox comboBox = new JComboBox();
+    for(String el:this.competitors){
+      comboBox.addItem(el);
+    }
+    return comboBox;
+  }
+
+  public void update_layout(int index) {
+    if(index == 1 && this.is_done_painting){
+      for(int i = 0; i < 4; i++){this.remove(4);}
+    }else if (this.is_done_painting) {
+      for(int i = 0; i < 3; i++){this.remove(4);}
+      addBottomStuff();
+    }
+    this.revalidate();
+    this.repaint();
+  }
+
+  public void addBottomStuff(){
+    this.add(this.firstCompLabel);
+    this.add(this.firstCompetitorSelector);
+    this.add(this.secondCompLabel);
+    this.add(this.secondCompetitorSelector);
+    this.add(this.thirdCompLabel);
+    this.add(this.thirdCompetitorSelector);
+    this.add(this.betValueLabel);
+    this.add(this.valueInput);
+    this.add(this.submit);
+  }
+
+  public int getType(){
+    return this.type;
   }
 }

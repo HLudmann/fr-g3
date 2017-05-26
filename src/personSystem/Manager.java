f84c6d69a9f1a9073a5602dc9be0d963c3988e3e
Bruno MATEU <bruno.mateu@telecom-bretagne.eu>
2017-05-26 12:18:30 +0200

    Player class completed, PlayerTest rebuilt

--- src/personSystem/Manager.java
+++ src/personSystem/Manager.java
@@ -5,7 +5,6 @@ import exceptions.IncorrectString;
 public class Manager extends SystemUser{
 
         public Manager(String firstName, String lastName, String nickname, String password) throws IncorrectString{
-
 					super(firstName, lastName, nickname, password);
         }
 
--- src/personSystem/Player.java
+++ src/personSystem/Player.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import betSystem.Bet;
 import exceptions.IncorrectString;
 import exceptions.InvalidWallet;
+import exceptions.ItemAlreadyInList;
+import exceptions.ItemNotInList;
 
 
 public class Player extends SystemUser {
@@ -11,7 +13,7 @@ public class Player extends SystemUser {
 	private ArrayList<Bet> betList;
 
 
-	public Player(String firstName, String lastName, String password, String nickname) throws IncorrectString{
+	public Player(String firstName, String lastName, String nickname, String password) throws IncorrectString{
 
 		super(firstName, lastName, password, nickname);
 
@@ -19,7 +21,8 @@ public class Player extends SystemUser {
 		wallet=0;
 	}
 
-	public Player(String firstName, String lastName, String password, String nickname, long wallet) throws IncorrectString{
+	public Player(String firstName, String lastName, String password, String nickname, long wallet) throws IncorrectString,
+																																																			InvalidWallet{
 		//define a custom value of wallet
 		this(firstName, lastName, password, nickname);
 		this.setWallet(wallet);
@@ -27,7 +30,7 @@ public class Player extends SystemUser {
 
 
 
-	public void setWallet(int w) throws InvalidWallet{
+	public void setWallet(long w) throws InvalidWallet{
 		if (w<0){
 			throw new InvalidWallet("wallet can't be negative");
 		}
@@ -42,21 +45,27 @@ public class Player extends SystemUser {
 		return betList;
 	}
 
-	public void addBet(Bet b) {
+	public void addBet(Bet b) throws ItemAlreadyInList{
 
 		if (!betList.contains(b)){
 
 			betList.add(b);
 
 		}
+		else{
+			throw new ItemAlreadyInList("Bet already in list");
+		}
 	}
 
-	public void removeBet(Bet b){
+	public void removeBet(Bet b) throws ItemNotInList{
 
 		if (betList.contains(b)){
 
 			betList.remove(b);
 
+		}
+		else{
+			throw new ItemNotInList("Can't remove a not-existing bet");
 		}
 	}
 

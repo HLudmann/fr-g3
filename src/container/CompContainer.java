cbd3a297df72b20ed1acbb05c6a3a5c7d97bad22
Hugo Selle <hugo-selle@hotmail.fr>
2017-05-29 10:29:02 +0200

    Update CompContainer

--- src/container/CompContainer.java
+++ src/container/CompContainer.java
@@ -11,32 +11,54 @@ import betSystem.*;
 import personSystem.Competitor;
 import exceptions.*;
 
+@Entity
+@NamedQuery(
+		name="selectEverything",
+		query="SELECT c FROM Competition c")
+
+/**
+ * @author Hsb511 
+ *
+ */
 public class CompContainer {
 	private ArrayList<Competition> compDB;
 
-
 	public CompContainer() {
 		EntityManager em = JPAUtil.getEntityManager();
+		
+		competitions = em.createNamedQuery("selectEverything").getResultList;
+		for (Object competition : competitions) {
+			Competition comp = (Competition)competition;
+			compDB.add(comp);				
+		}
 	}
 
 	
 	/**
 	 * Method to add a Competition to the DataBase
+	 * @param name						String which is the primary key
+	 * @param date						java.util.Calendar
+	 * @param competitors				array of Competitor
+	 * @throws BadParametersException
 	 */
 	public void addComp(String name, Calendar date, Competitor[] competitors) throws BadParametersException {
 		EntityManager em = JPAUtil.getEntityManager();
 		try {
 			Competition c = new Competition(name, date, competitors);
 			em.persist(c);
+			compDB.add(c);
 		} catch (Exception e) {
 			throw new BadParametersException();
 		}
 	}
-	
-	/** 
-	 * Method to update a Competition in the DataBase /!\ you can't update the primary key name
-	 */
 
+	
+	/**
+	 * Method to update a Competition in the DataBase /!\ you can't update the primary key name
+	 * @param name						String which is the primary key
+	 * @param date						java.util.Calendar
+	 * @throws BadParametersException
+	 */
 	public void updateComp(String name, Calendar date) throws BadParametersException {
 		EntityManager em = JPAUtil.getEntityManager();
 		ArrayList<Competition> searchRes = findCompetitionByName(name);	
@@ -45,14 +67,20 @@ public class CompContainer {
 		} 
 		try  {
 			Competition c = searchRes.get(0);
+			compDB.remove(c);
 			c.setDate(date);
 			em.merge(c);
+			compDB.add(c);
 		} catch (Exception e) {
 			throw new BadParametersException();
 		}
 	}
 	
 	
+	/**
+	 * @param name		String which is the primary key
+	 * @throws BadParametersException
+	 */
 	public void delComp(String name) throws BadParametersException {
 		EntityManager em = JPAUtil.getEntityManager();
 		ArrayList<Competition> searchRes = findCompetitionByName(name);	
@@ -62,15 +90,17 @@ public class CompContainer {
 		try {
 			Competition c = searchRes.get(0);
 			em.remove(c);
+			compDB.remove(c);
 		} catch (Exception e) {
 			throw new BadParametersException();
 		}
-		return true;
 	}
 
 	
 	/**
-	 * Methods to search a Competition in the Database
+	 * Method to find Competitions by their name
+	 * @param name 		String which is the primary key of the table Competition
+	 * @return 			ArrayList of Competitions
 	 */
 	public ArrayList<Competition> findCompetitionByName(String name) {
 		ArrayList<Competition> res = new ArrayList<Competition>();
@@ -84,6 +114,12 @@ public class CompContainer {
 		return res;
 	}
 
+	
+	/**
+	 * Method to find Competitions by their Date
+	 * @param date 		the date of the Competition of type Calendar			
+	 * @return 			ArrayList of Competitions
+	 */
 	public ArrayList<Competition> findCompetitionByDate(Calendar date) {
 		ArrayList<Competition> res = new ArrayList<Competition>();
 		Iterator<Competition> it = this.compDB.iterator();
@@ -96,6 +132,10 @@ public class CompContainer {
 		return res;
 	}
 
+	/**
+	 * Method to get the Competitions which are not finished
+	 * @return ArrayList of Competitions
+	 */
 	public ArrayList<Competition> getCompetitionsNotEnded() {
 		ArrayList<Competition> res = new ArrayList<Competition>();
 		Iterator<Competition> it = this.compDB.iterator();
@@ -108,8 +148,11 @@ public class CompContainer {
 		return res;
 	}
 
+	/**
+	 * Method to get all the Competitions
+	 * @return ArrayList of Competitions
+	 */
 	public ArrayList<Competition> getCompetitions() {
 		return this.compDB;
 	}
-	
 }

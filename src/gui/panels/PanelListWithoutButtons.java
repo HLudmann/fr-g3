791b898cd60c5bca4454fc8d48607099f5cb17e9
JB Valladeau <jbvalladeau@gmail.com>
2017-06-06 14:06:53 +0200

    lists without editon and deletion options

---
+++ src/gui/panels/PanelListWithoutButtons.java
@@ -0,0 +1,49 @@
+package gui.panels;
+
+import gui.buttons.EditButton;
+import gui.buttons.DeleteButton;
+import gui.buttons.GoBackButton;
+
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.awt.Dimension;
+import java.util.ArrayList;
+
+@SuppressWarnings("serial")
+public class PanelListWithoutButtons extends JPanel {
+  public PanelListWithoutButtons(int type){
+
+    //Aesthetic borders
+    this.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+  //the following is just for testing purpose
+    ArrayList<ArrayList<String>> list = new ArrayList<ArrayList<String>>();
+
+    ArrayList<String> el = new ArrayList<String>();
+    el.add("Jean-Baptiste Valladeau");
+    el.add("jbvallad");
+    list.add(el);
+
+    el = new ArrayList<String>();
+    el.add("Jean Machin");
+    el.add("jmachin");
+    list.add(el);
+
+    //Creates a good layout for our list
+    GridLayout gl = new GridLayout();
+    gl.setColumns(2);
+    gl.setRows(list.size()+1);
+    gl.setHgap(5);
+    gl.setVgap(5);
+    this.setLayout(gl);
+
+    //Adds buttons related to the data provided
+    for (int i=0; i < list.size(); i++){
+      this.add(new JLabel(list.get(i).get(0)));
+    }
+    this.add(new GoBackButton("Retour", 1, 1));
+  }
+}
